8f8abb751adf04d8a537fd3f1f858f2436a98b0d
brunacgr <brunacg@gmail.com>
2022-11-15 15:33:24 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DS Meta" />
+                <h1>DS Meta</h1>
+                <p>Desenvolvido por 
+                    <a href="https://www.instagram.com/papersncoffee/"> Bruna Resende</a>
+                </p>
+            </div>
+        </header>
+
+    )
+}
+
+export default Header
